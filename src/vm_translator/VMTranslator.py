b34a6660822735e54d85b3c3258282eceb2a1fa9
Eric Ihli <ericihli@gmail.com>
2018-07-20 07:53:14 -0700

    Refactor to accept a directory
    TODO: Handle initialization of memory
    segments.

--- src/vm_translator/VMTranslator.py
+++ src/vm_translator/VMTranslator.py
@@ -2,6 +2,7 @@ import os
 import re
 import sys
 import parse
+import pathlib
 
 
 def strip_comments(line):
@@ -10,14 +11,31 @@ def strip_comments(line):
 
 def main(filename):
     result = []
+    path = pathlib.Path(filename)
+    if path.is_dir():
+        dir_name = path
+        filenames = path.iterdir()
+        for filename in filenames:
+            file_path = pathlib.Path(filename)
+            if file_path.suffix == '.vm':
+                result += [writefile(filename)]
+    else:
+        dir_name = path.parent
+        result += [writefile(filename)]
+
+    with open('%s/%s.asm' % (dir_name, path.stem), 'w') as f:
+        f.write('\n'.join(result))
+
+
+def writefile(filename):
+    result = []
     fn = os.path.basename(filename).split('.')[0]
     with open(filename) as f:
         for line in f:
             line = strip_comments(line).strip()
             if line:
                 result += parse.parse_cmd(line, fn)
-    with open('%s/%s.asm' % (os.path.dirname(filename), fn), 'w') as f:
-        f.write('\n'.join(result))
+    return '\n'.join(result)
 
 
 if __name__ == '__main__':
