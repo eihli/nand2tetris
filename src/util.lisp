d8aa168e6a49af0fc70c554c23fa1bf92d7f9cde
Eric Ihli <ericihli@gmail.com>
2018-08-19 14:15:26 -0700

    Add a debug util shortcut

--- src/util.lisp
+++ src/util.lisp
@@ -1,6 +1,20 @@
-(defun run-test ()
-  (interactive)
-  (let ((default-directory "~/code/nand2tetris/src/jack_analyzer/"))
-    (async-shell-command "python -m unittest discover")))
+(defun run-test (dir test)
+  (let ((default-directory dir ))
+    (async-shell-command
+     (concat "python -m unittest " test))))
 
-(global-set-key (kbd "C-c t") 'run-test)
+(defun run-compiler-test ()
+  (interactive)
+  (run-test
+   "~/code/nand2tetris/src/jack_analyzer"
+   "test_compiler.TestSubroutineDec"))
+
+(defun run-pdb (test)
+  (let ((default-directory "~/code/nand2tetris/src/jack_analyzer"))
+    (pdb
+     (concat "python -m unittest " test))))
+
+(global-set-key (kbd "C-c t") 'run-compiler-test)
+(global-set-key
+ (kbd "C-c d")
+ (lambda () (interactive) (run-pdb "test_compiler.TestSubroutineDec")))
